5842a9f1f9f6fab2cb4edbd5013ebc2c9b5b2e3f
Aldebaran64 <aldebaran26adhitya@gmail.com>
2025-10-23 02:01:43 +0700

    Revert "Draft 1: Venue Module"
    This reverts commit e986914f33a017e7c47566aa696c3f08e463fd16.

--- main/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from main.models import Venue
-
-class VenueForm(ModelForm):
-    class Meta:
-        model = Venue
-        fields = ["CityName", "StreetName", "leisure", "name"]
--- velp/settings.py
+++ velp/settings.py
@@ -33,9 +33,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "aldebaran-rahman-velp.pbp.cs.ui.ac.id"]
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://aldebaran-rahman-velp.pbp.cs.ui.ac.id"
-]
 
 # Application definition
 
@@ -46,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
 ]
 
 MIDDLEWARE = [
@@ -64,7 +60,7 @@ ROOT_URLCONF = 'velp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
