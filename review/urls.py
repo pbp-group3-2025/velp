d1a9c58df5a5dacb3fd75fc231b9d30586575f00
FannyDemarin <fademarin@gmail.com>
2025-12-19 16:57:05 +0700

    Update review urls

--- review/urls.py
+++ review/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('edit/<int:review_id>/', views.edit_review, name='edit_review'),
     path('add_review_ajax/<uuid:venue_id>/', views.add_review_with_ajax, name='add_review_with_ajax'),
     path('edit_review_ajax/<int:review_id>/', views.edit_review_ajax, name='edit_review_ajax'),
+    path('venue/<int:venue_id>/json/', views.get_reviews_by_venue_json, name='get_reviews_json'),
+    path('add_review_ajax/<int:venue_id>/', views.add_review_ajax_flutter, name='add_review_ajax_flutter'),
+    path('edit_review_ajax/<int:review_id>/', views.edit_review_ajax_flutter, name='edit_review_ajax_flutter'),
+    path('delete_review/<int:review_id>/', views.delete_review_flutter, name='delete_review_flutter'),
+
 ]
